6545e647a6cf6b4ad48753ff14e24b365119b279
sonika agrawal <sonika@saurabhs-mbp.lan>
2020-07-11 12:42:52 -0400

    inital commit

--- Terraform_rg.tf
+++ Terraform_rg.tf
@@ -1,11 +1,12 @@
- provider "azurerm" {
+provider "azurerm" {
     version = 1.38
     }
 
 # Create New Resource Group in Azure Subscription
-resource "azurerm_resouce_group" "somildebate_id" {
-    name                = "somildebate"
-    location            = "East US"
+resource "azurerm_resource_group" "somildebate1_id" {
+    name                = "somildebate1"
+ location            = "East US"
 }
+
 
 
